95803ed9b4d46917912277790ee70af1a7bb86ac
yohei <moritayouheida@gmail.com>
2022-08-28 08:51:05 +0900

    number 22

---
+++ basic/main.js
@@ -0,0 +1,26 @@
+let count = 0;
+count = 30;
+let newCount = 0;
+const daysInWeek = 7;
+
+let additionResult = 2 + 5;
+let result = 5;
+result = result + 10;
+result += 10;
+
+const userName = "Yohei";
+let string = "hello" + userName;
+string = `hello\n${userName}`;
+// console.log(string);
+
+const userInput = "10.9";
+let calcResult;
+calcResult = Number(userInput) + 10;
+calcResult = parseInt(userInput) + 10;
+calcResult = parseFloat(userInput) + 10;
+calcResult = +userInput + 10;
+
+const tenNumber = 10;
+calcResult = "10" + String(tenNumber);
+calcResult = "10" + tenNumber.toString();
+console.log(calcResult);
